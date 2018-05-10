974a95d0ee7d0a13fea7da481bcb4df3d9058e11
merritt Webb <merrittwebb@merritts-MacBook-Pro.local>
2018-05-10 13:53:49 -0500

    uploaded

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+
+let connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "", // Enter your password here
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Plain Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('FAT Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('mayo and mustard Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Kansas city Cheeseburger');
